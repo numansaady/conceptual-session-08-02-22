5af252ec9a0b76688927808ad711a97ac3a7216e
Numan Saady <numansaady@gmail.com>
2022-02-09 20:33:06 +0600

    count added

--- main.js
+++ main.js
@@ -1,10 +1,46 @@
-const btnGroup = document.getElementsByClassName("btn");
-for (const btn of btnGroup) {
-  btn.addEventListener("click", function (e) {
-    if (e.target.id == "first-btn") {
-      document.getElementById("heading").style.color = "red";
-    } else {
-      document.getElementById("heading").style.color = "green";
-    }
-  });
+{
+  const btnGroup = document.getElementsByClassName("btn");
+  for (const btn of btnGroup) {
+    btn.addEventListener("click", function (e) {
+      if (e.target.id == "first-btn") {
+        document.getElementById("heading").style.color = "red";
+      } else {
+        document.getElementById("heading").style.color = "green";
+      }
+    });
+  }
+}
+
+{
+  // Count
+  let count = 0;
+  //   document.getElementById("plus").addEventListener("click", function (e) {
+  //     // count = count + 1;
+  //     // count += 1;
+  //     count++;
+  //     // document.getElementById(id).innerText = count;   or using function
+  //     output("count");
+  //   });
+  //   document.getElementById("minus").addEventListener("click", function (e) {
+  //     // count = count - 1;
+  //     // count -= 1;
+  //     count--;
+  //     // document.getElementById(id).innerText = count;  or using function
+  //     output("count");
+  //   });
+
+  // Alternaive Short format of count
+
+  function add(e) {
+    count++;
+    output("count");
+  }
+  function minus(e) {
+    count--;
+    output("count");
+  }
+
+  function output(id) {
+    document.getElementById(id).innerText = count;
+  }
 }
